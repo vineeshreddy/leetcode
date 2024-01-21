8abdb82d603ded60aec8e0f9db0792afcca933a4
vineeshreddy <40059295+vineeshreddy@users.noreply.github.com>
2024-01-21 00:22:09 -0600

    Time: 2 ms (71.15%) | Memory: 54.3 MB (19.25%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -5,14 +5,14 @@ class Solution {
         int[] postfix = new int[nums.length];
         prefix[0] = 1;
         for(int i=1; i<nums.length; i++) {
-            // 5 4 3 2 1 (nums)
-            // 1 5 20 60 120(prefix)
+            // 5 4 3 2 1 (nums) 1 2 3 4
+            // 1 5 20 60 120(prefix) 1 1 2 6
             prefix[i] = prefix[i-1] * nums[i-1];
         }
         postfix[nums.length-1] = 1;
         for(int i=nums.length-2; i>=0; i--) {
-            // 5 4 3 2 1(nums)
-            // 24 6 2 1 1(postfix)
+            // 5 4 3 2 1(nums) 1 2 3 4
+            // 24 6 2 1 1(postfix)   24 12 4 1
             postfix[i] = postfix[i+1] * nums[i+1];
         }
         for(int i=0; i<nums.length; i++) {
@@ -21,3 +21,26 @@ class Solution {
         return postfix;    
     }
 }
+/*
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // while considering the 0th index excpet that index reaming all indexes are multiplied
+        // Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        for(int i=1; i<nums.length; i++) {
+            // 5 4 3 2 1 (nums) 1 2 3 4
+            // 1 5 20 60 120(prefix) 1 1 2 6
+            output[i] = output[i-1] * nums[i-1];
+        }
+        int R=1;
+        for(int i=nums.length-1; i>=0; i--) {
+
+            // 1 1 2 6
+            output[i] = output[i] * R; //  6*1=6 // 2*4=8 // 12*1=12 // 24 *1=24
+            R = R * nums[i]; 1 2 3 4  //  1* 4 =4  // 4*3= 12 // 12 * 2 = 24 // 
+        }
+        return output;    
+    }
+}
+*/
